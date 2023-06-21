dba3b2606ab8914dbec5f99055b37a7af97ade56
silver-sv <128167816+silver-sv@users.noreply.github.com>
2023-06-21 08:25:25 +0000

    Sample html

--- main.js
+++ main.js
@@ -766,3 +766,128 @@ $checkboxes.on('click', function(){
 });
 
 // 
+
+
+// // // // // // //  task-10 // // // // // //
+1
+
+let names = {
+  first: "Tom",
+  second: "Sam",
+  third: "Ray",
+  fourth: "Bob",
+};
+
+let { first: f, third: x, fifth = "Name №5" } = names;
+
+console.log(f); // "Tom"
+console.log(x); // "Ray"
+console.log(fifth); // "Name №5"
+
+// 
+
+2
+
+let data = {
+  names: ["Sam", "Tom", "Ray", "Bob"],
+  ages: [20, 24, 22, 26],
+};
+
+let { names: [name1, name2, name3, name4], ages: [age1, age2, age3, age4] } = data;
+
+console.log(name2); // "Tom"
+console.log(age2); // 24
+console.log(name4); // "Bob"
+console.log(age4); // 26
+
+// 
+
+3
+
+function mul(...args) {
+  let result = 1;
+
+  for (let i = 0; i < args.length; i++) {
+    if (typeof args[i] === 'number') {
+      result *= args[i];
+    }
+  }
+
+  return result;
+}
+
+console.log(mul(1, "str", 2, 3, true)); // 6
+console.log(mul(null, "str", false, true)); // 0
+
+// 
+
+4
+
+let server = {
+  data: 0,
+  convertToString: function (callback) {
+    callback(() => {
+      return this.data + "";
+    });
+  }
+};
+
+let client = {
+  server: server,
+  result: "",
+  calc: function (data) {
+    this.server.data = data;
+    this.server.convertToString(this.notification());
+  },
+  notification: function () {
+    return () => {
+      this.result = callback();
+    };
+  }
+};
+
+client.calc(123);
+console.log(client.result); // "123"
+console.log(typeof client.result); // "string"
+
+// 
+
+5
+
+function mapBuilder(keysArray, valuesArray) {
+  if (keysArray.length !== valuesArray.length) {
+    throw new Error('The length of keysArray and valuesArray must be the same.');
+  }
+
+  let map = new Map();
+
+  for (let i = 0; i < keysArray.length; i++) {
+    map.set(keysArray[i], valuesArray[i]);
+  }
+
+  return map;
+}
+
+let keys = [1, 2, 3, 4];
+let values = ["div", "span", "b", "i"];
+let map = mapBuilder(keys, values);
+
+console.log(map.size); // 4
+console.log(map.get(2)); // "span"
+
+// 
+
+6
+
+let arr = [];
+
+for (var i = 0; i <= 2; i++) {
+  arr[i] = (function (num) {
+    return function () {
+      console.log(num);
+    };
+  })(i);
+}
+
+arr[0](); // 0
+arr[arr.length - 1](); // 2
